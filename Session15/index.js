677983d770b344fa657c26c0516c2118f7d14380
Kilordye <carldominiksalazar@gmail.com>
2026-03-12 19:48:07 +0800

    Added Session 15

---
+++ Session15/index.js
@@ -0,0 +1,13 @@
+// Mini-activity1
+// Using prompt convert Celsius to Kelvin
+// Present the output using alert
+let celsius = parseFloat(prompt("Please enter the temperature in Celsius: "));
+let kelvin = parseFloat(celsius) + 273.15;
+alert("The temperature in Kelvin is: " + kelvin);
+
+// Mini-activity2
+// Using prompt get circle area
+// Present the output using alert
+let radius = parseFloat(prompt("Please enter the radius of the circle: "));
+let area = Math.PI * Math.pow(radius, 2);
+alert("The area of the circle is: " + area);
